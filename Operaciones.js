f0ad2b0b7365b10d7fa5f7dcb920e0fdfc71d63f
Axalli2901 <xochitl.axalli@gmail.com>
2025-09-19 09:58:03 -0600

    Add files via upload

---
+++ Operaciones.js
@@ -0,0 +1,17 @@
+// 1. Multiplicación (función clásica)
+function multiplicaC(a, b) {
+  return a * b;
+}
+
+// 2. Multiplicación (arrow function)
+const multiplicarArrow = (a, b) => a * b;
+console.log(multiplicarArrow(5, 6));
+
+// 3. Elevar al cuadrado (función clásica)
+function cuadrado(c) {
+  return c * c;
+}
+
+// 4. Elevar al cuadrado (arrow function)
+const cuadradoArrow = c => c * c;
+console.log(cuadradoArrow(7));      
